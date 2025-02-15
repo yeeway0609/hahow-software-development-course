c66bc3b6407321eab1ad07043ebc23253d0d3651
yeeway <yiwei.suuu@gmail.com>
2025-02-16 02:28:23 +0800

    feat: add input validation for CreateOrder API and implement request handling logic

--- order_system/handler/create_order.py
+++ order_system/handler/create_order.py
@@ -1,10 +1,11 @@
 import json
 
 from dependency_injector.wiring import Provide, inject
-from flask import jsonify, request
+from flask import current_app, request, jsonify
 from flask.views import MethodView
 
 from order_system.database.order_collection_dao import OrderCollectionDAO
+from order_system.exception import InvalidAPIUsageException
 
 
 class CreateOrderHandler:
@@ -12,17 +13,48 @@ class CreateOrderHandler:
     def __init__(self, order_collection_dao: OrderCollectionDAO):
         self.__order_collection_dao = order_collection_dao
 
+    @staticmethod
+    def validate_input(request_body: json):
+        """確認 input 是否符合我們 API 的定義
+
+        :param request_body:
+        :return:
+        """
+        allowed_fields = {"customerName", "orderTime", "items"}
+        for key in request_body.keys():
+            # 輸入的欄位不在 allowed_fields 中
+            if key not in allowed_fields:
+                error_msg = "Request has unrecognized field: " + key
+                current_app.logger.error(error_msg)
+                raise InvalidAPIUsageException(
+                    error_type="Invalid Input", message=error_msg, status_code=400
+                )
+            # 輸入的 customerName, orderTime, items 欄位為空值
+            if key in {"customerName", "orderTime", "items"}:
+                if not request_body.get(key):
+                    error_msg = f"Request has empty {key} value"
+                    raise InvalidAPIUsageException(
+                        error_type="Invalid Input", message=error_msg, status_code=400
+                    )
+
     def handle_request(self, request_body: json):
-        # 這兩行只是為了暫時讓 code 通過 pylint，以方便在課堂上進行章節６的示範
-        # 同學在完成這個 function 後請將這兩行刪除
-        assert self.__order_collection_dao is not None
-        assert request_body is not None
-        # ------------------------------
+        """處理 CreateOrderAPI 的邏輯
 
-        # 完成這個 function
+        :param request_body: CreateOrderInput
+        :return: CreateOrderOutput 符合我們 API 定義的 dict
+        """
+        current_app.logger.info("Received request: " + str(request_body))
 
-        raise NotImplementedError
+        self.validate_input(request_body)
 
+        order_id = self.__order_collection_dao.create_order_data(
+            order=request_body
+        )
+
+        response = {"order_id": str(order_id)}
+        current_app.logger.info("Successfully created order: " + str(response))
+
+        return response
 
 class CreateOrderView(MethodView):
     def __init__(
---
+++ tst/unit/handler/test_create_order.py
@@ -0,0 +1,55 @@
+import json
+from unittest.mock import create_autospec, Mock
+
+import pytest
+
+from order_system import InvalidAPIUsageException
+from order_system.database.order_collection_dao import OrderCollectionDAO
+from order_system.handler.create_order import CreateOrderHandler
+
+def test_validate_input_when_succeed(test_app_context):
+    with test_app_context():
+        try:
+            CreateOrderHandler.validate_input({
+                "customerName": "test_customer",
+                "orderTime": "test_time",
+                "items": ["item1", "item2"]
+            }) # 測試輸入符合格式的值
+        except InvalidAPIUsageException:
+            pytest.fail("Unexpected InvalidAPIUsageException ..")
+
+def test_validate_input_when_failed(test_app_context):
+    with test_app_context():
+        with pytest.raises(InvalidAPIUsageException):
+            CreateOrderHandler.validate_input({
+                "bad_field": ""
+            }) # 測試輸入不存在的欄位
+        with pytest.raises(InvalidAPIUsageException):
+            CreateOrderHandler.validate_input({
+                "customerName": "",
+                "orderTime": "",
+                "items": []
+            }) # 測試輸入空值
+        with pytest.raises(InvalidAPIUsageException):
+            CreateOrderHandler.validate_input({
+                "customerName": "test_customer",
+                "orderTime": "test_time",
+                "items": ["item1"],
+                "extra": ""
+            }) # 測試輸入多餘的欄位
+
+def test_create_order_when_call_succeed(test_app_context):
+    with test_app_context():
+        mock_order_dao = create_autospec(OrderCollectionDAO)
+        mock_order_dao.create_order_data = Mock(return_value="test_order_id")
+        create_order_handler = CreateOrderHandler(mock_order_dao)
+
+        request_body = {
+            "customerName": "test_customer",
+            "orderTime": "test_time",
+            "items": ["item1", "item2"]
+        }
+
+        expected = {"order_id": "test_order_id"}
+        assert create_order_handler.handle_request(request_body) == expected
+        mock_order_dao.create_order_data.assert_called_once_with(order=request_body)
