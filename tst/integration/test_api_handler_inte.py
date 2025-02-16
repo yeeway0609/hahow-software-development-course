cfb7124d83327f20596a2b1ea648b30692c317c7
yeeway <yiwei.suuu@gmail.com>
2025-02-16 14:19:40 +0800

    feat: add GetOrderHandler and CreateOrderHandler to integration tests

--- tst/integration/test_api_handler_inte.py
+++ tst/integration/test_api_handler_inte.py
@@ -6,7 +6,10 @@ from pymongo import MongoClient
 
 from order_system import create_app
 from order_system.database.menu_collection_dao import MenuCollectionDAO
+from order_system.database.order_collection_dao import OrderCollectionDAO
 from order_system.handler.get_menu import GetMenuHandler
+from order_system.handler.get_order import GetOrderHandler
+from order_system.handler.create_order import CreateOrderHandler
 
 
 class MockContainer(containers.DeclarativeContainer):
@@ -18,9 +21,18 @@ class MockContainer(containers.DeclarativeContainer):
     db_client = providers.Object(mock_db)
 
     menu_collection_dao = providers.Callable(create_autospec, MenuCollectionDAO)
+    order_collection_dao = providers.Callable(create_autospec, OrderCollectionDAO)
 
     get_menu_handler = providers.Factory(
         GetMenuHandler, menu_collection_dao=menu_collection_dao
+    )
+
+    get_order_handler = providers.Factory(
+        GetOrderHandler, order_collection_dao=order_collection_dao
+    )
+
+    create_order_handler = providers.Factory(
+        CreateOrderHandler, order_collection_dao=order_collection_dao
     )
 
 
